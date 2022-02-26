2737d1af8cd20be2f24a71c45dc5157c663b3ea9
Ciberian <isi04047891@gmail.com>
2022-02-26 17:37:40 +0500

    рефакторит функции на случай передачи в них отрицательных аргументов

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 const getRandomInt = (firstNumber, secondNumber) => {
-  const max = Math.max(firstNumber, secondNumber);
-  const min = Math.min(firstNumber, secondNumber);
+  const max = Math.floor(Math.max(Math.abs(firstNumber), Math.abs(secondNumber)));
+  const min = Math.ceil(Math.min(Math.abs(firstNumber), Math.abs(secondNumber)));
 
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  const result = Math.random() * (max - min + 1) + min;
+
+  return Math.floor(result);
 };
 
 getRandomInt(10, 20);
 
 const getRandomFloat = (firstNumber, secondNumber, decimals) => {
-  const max = Math.max(firstNumber, secondNumber);
-  const min = Math.min(firstNumber, secondNumber);
+  const max = Math.max(Math.abs(firstNumber), Math.abs(secondNumber));
+  const min = Math.min(Math.abs(firstNumber), Math.abs(secondNumber));
 
-  const intermediateResult = Math.random() * (max - min + 1) + min;
+  const result = Math.random() * (max - min + 1) + min;
 
-  return Number(Math.min(intermediateResult, max).toFixed(decimals).toString());
+  return Number(Math.min(result, max).toFixed(decimals).toString());
 };
 
 getRandomFloat(3.123456789123, 1.123, 10);
