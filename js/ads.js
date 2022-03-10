2269ad3a53b2b1d2f8c37da1254893c13e5ebf45
Ciberian <isi04047891@gmail.com>
2022-03-11 02:47:44 +0500

    проводит потный рефакторинг в файле ads.js

--- js/ads.js
+++ js/ads.js
@@ -5,117 +5,116 @@ const mapCanvas = document.querySelector('#map-canvas');
 const templateFragment = document.querySelector('#card').content;
 const template = templateFragment.querySelector('.popup');
 const fragment = document.createDocumentFragment();
+const offerType = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель'
+};
 
-similarAds.forEach((similarAd) => {
-  const ad = template.cloneNode(true);
+const ONE_ITEM = 1;
+const TWO_ITEMS = 2;
+const FOUR_ITEMS = 4;
+const TEN_ITEMS = 10;
+const ELEVEN_ITEMS = 11;
+const TWENTY_ITEMS = 20;
+const DIVIDER_TEN = 10;
+const DIVIDER_ONE_HUNDRED = 100;
 
-  if (similarAd.author.avatar) {
-    ad.querySelector('.popup__avatar').src = similarAd.author.avatar;
-  } else {
-    ad.querySelector('.popup__avatar').remove();
+const pluralizeRus = (number, wordEndForms) => {
+  if (number % DIVIDER_TEN === ONE_ITEM && number % DIVIDER_ONE_HUNDRED !== ELEVEN_ITEMS) {
+    return wordEndForms[0];
   }
 
-  if (similarAd.offer.title) {
-    ad.querySelector('.popup__title').textContent = similarAd.offer.title;
-  } else {
-    ad.querySelector('.popup__title').remove();
+  if (number % DIVIDER_TEN >= TWO_ITEMS && number % DIVIDER_TEN <= FOUR_ITEMS && (number % DIVIDER_ONE_HUNDRED < TEN_ITEMS || number % DIVIDER_ONE_HUNDRED >= TWENTY_ITEMS)) {
+    return wordEndForms[1];
   }
 
-  if (similarAd.offer.address) {
-    ad.querySelector('.popup__text--address').textContent = similarAd.offer.address;
+  return wordEndForms[2];
+};
+
+const insertData = (node, data, prop='textContent') => {
+  if (data) {
+    node[prop] = data;
   } else {
-    ad.querySelector('.popup__text--address').remove();
+    node.remove();
   }
+};
 
-  if (similarAd.offer.price) {
-    ad.querySelector('.popup__text--price').textContent = `${similarAd.offer.price} ₽/ночь`;
+const insertCapacityData = (node, roomsData, guestsData) => {
+  if (roomsData && guestsData) {
+    node.textContent = `${roomsData} комнат${pluralizeRus(roomsData, ['а', 'ы', ''])} для ${guestsData} гост${pluralizeRus(guestsData, ['я', 'ей', 'ей'])}`;
+  } else if (roomsData && !guestsData) {
+    node.textContent = `${roomsData} комнат${pluralizeRus(roomsData, ['а', 'ы', ''])}`;
+  } else if (!roomsData && guestsData) {
+    node.textContent = `Для ${guestsData} гост${pluralizeRus(guestsData, ['я', 'ей', 'ей'])}`;
   } else {
-    ad.querySelector('.popup__text--price').remove();
+    node.remove();
   }
+};
 
-  const offerType = {
-    palace: 'Дворец',
-    flat: 'Квартира',
-    house: 'Дом',
-    bungalow: 'Бунгало',
-    hotel: 'Отель'
-  };
-
-  if (similarAd.offer.type) {
-    ad.querySelector('.popup__type').textContent = offerType[similarAd.offer.type];
+const insertTimeData = (node, checkInData, checkOutData) => {
+  if (checkInData && checkOutData) {
+    node.textContent = `Заезд после ${checkInData}, выезд до ${checkOutData}`;
+  } else if (checkInData && !checkOutData) {
+    node.textContent = `Заезд после ${checkInData}`;
+  } else if (!checkInData && checkOutData) {
+    node.textContent = `Выезд до ${checkOutData}`;
   } else {
-    ad.querySelector('.popup__type').remove();
+    node.remove();
   }
+};
 
-  const guestsRus = (similarAd.offer.guests % 10 !== 1 || similarAd.offer.guests === 11) ? 'гостей' : 'гостя';
-
-  const pluralizeRus = (n, forms) => {
-    if (n % 10 === 1 && n % 100 !== 11) {
-      return forms[0];
-    }
-
-    if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) {
-      return forms[1];
-    }
-
-    return forms[2];
-  };
-
-  const capacity = ad.querySelector('.popup__text--capacity');
-  const {rooms, guests} = similarAd.offer;
-
-  if (rooms && guests) {
-    capacity.textContent = `${rooms} комнат${pluralizeRus(rooms, ['а', 'ы', ''])} для ${guests} ${guestsRus}`;
-  } else if (rooms && !guests) {
-    capacity.textContent = `${rooms} комнат${pluralizeRus(rooms, ['а', 'ы', ''])}`;
-  } else if (!rooms && guests) {
-    capacity.textContent = `Для ${guests} ${guestsRus}`;
-  } else {
-    capacity.remove();
-  }
-
-  const time = ad.querySelector('.popup__text--time');
-  const {checkin, checkout} = similarAd.offer;
-
-  if (checkin && checkout) {
-    time.textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
-  } else if (checkin && !checkout) {
-    time.textContent = `Заезд после ${checkin}`;
-  } else if (!checkin && checkout) {
-    time.textContent = `Выезд до ${checkout}`;
-  } else {
-    time.remove();
-  }
-
-  const featuresList = ad.querySelectorAll('.popup__feature');
-  const {features} = similarAd.offer;
-
-  featuresList.forEach((featuresListItem) => {
-    const hasData = features.some(
-      (feature) => featuresListItem.classList.contains(`popup__feature--${feature}`),
+const makeFeatureList = (featureNodes, featureData) => {
+  featureNodes.forEach((featureNodesItem) => {
+    const hasData = featureData.some(
+      (feature) => featureNodesItem.classList.contains(`popup__feature--${feature}`),
     );
 
     if (!hasData) {
-      featuresListItem.remove();
+      featureNodesItem.remove();
     }
   });
+};
 
-  if (similarAd.offer.description) {
-    ad.querySelector('.popup__description').textContent = similarAd.offer.description;
-  } else {
-    ad.querySelector('.popup__description').remove();
-  }
-
-  const photos = similarAd.offer.photos.map((photo) => `<img src="${photo}"class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
-
-  if (photos.join()) {
-    const container = ad.querySelector('.popup__photos');
-    const templateImg = container.querySelector('img');
+const makeGallery = (node, photoListData) => {
+  if (photoListData.join()) {
+    const templateImg = node.querySelector('img');
     templateImg.remove();
-    container.insertAdjacentHTML('afterbegin', photos.join(''));
+    node.insertAdjacentHTML('afterbegin', photoListData.join(''));
   } else {
-    ad.querySelector('.popup__photos').remove();
+    node.remove();
   }
+};
+
+similarAds.forEach((similarAd) => {
+  const ad = template.cloneNode(true);
+  const avatar = ad.querySelector('.popup__avatar');
+  const title = ad.querySelector('.popup__title');
+  const address = ad.querySelector('.popup__text--address');
+  const price = ad.querySelector('.popup__text--price');
+  const type = ad.querySelector('.popup__type');
+  const description = ad.querySelector('.popup__description');
+  const capacity = ad.querySelector('.popup__text--capacity');
+  const {rooms, guests} = similarAd.offer;
+  const time = ad.querySelector('.popup__text--time');
+  const {checkin, checkout} = similarAd.offer;
+  const featuresList = ad.querySelectorAll('.popup__feature');
+  const {features} = similarAd.offer;
+  const galleryContainer = ad.querySelector('.popup__photos');
+  const photoList = similarAd.offer.photos.map((photo) => `<img src="${photo}"class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+
+  insertData(avatar, similarAd.author.avatar, 'src');
+  insertData(title, similarAd.offer.title);
+  insertData(address, similarAd.offer.address);
+  insertData(price, `${similarAd.offer.price} ₽/ночь`);
+  insertData(type, offerType[similarAd.offer.type]);
+  insertData(description, similarAd.offer.description);
+  insertCapacityData(capacity, rooms, guests);
+  insertTimeData(time, checkin, checkout);
+  makeFeatureList(featuresList, features);
+  makeGallery(galleryContainer, photoList);
 
   fragment.appendChild(ad);
 });
