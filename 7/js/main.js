111c395e6e3d550fea8cbed5a27572f65d6a7c83
Keks <keks@htmlacademy.ru>
2022-03-14 20:33:11 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,6 @@
+import './ads.js';
+import './user-form.js';
+import {disableForms, enableForms} from './state.js';
+
+disableForms();
+enableForms();
---
+++ 7/js/user-form.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('.ad-form');
+const rooms = form.querySelector('#room_number');
+const capacity = form.querySelector('#capacity');
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'error-text',
+});
+
+pristine.addValidator(form.querySelector('#price'), (value) => value <= 100000, 'Не больше 100 000 руб.');
+
+const validateCapacity = () => {
+  const roomsCount = rooms.querySelector('option:checked');
+  const guestsCount = capacity.querySelector('option:checked');
+
+  if (roomsCount.value === '100' && guestsCount.value !== '0') {
+    return false;
+  }
+
+  if (roomsCount.value === '100' && guestsCount.value === '0') {
+    return true;
+  }
+
+  if (roomsCount.value !== '100' && guestsCount.value !== '0') {
+    return +roomsCount.value >= +guestsCount.value;
+  }
+};
+
+const capacityErrorMessage = () => {
+  const roomsCount = rooms.querySelector('option:checked');
+  const guestsCount = capacity.querySelector('option:checked');
+
+  if (roomsCount.value === '100') {
+    return 'Не для гостей';
+  }
+
+  if (guestsCount.value === '0') {
+    return 'Укажите количество гостей';
+  }
+
+  return 'Количество гостей не должно превышать количество комнат';
+};
+
+pristine.addValidator(capacity, validateCapacity, capacityErrorMessage);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  pristine.validate();
+});
