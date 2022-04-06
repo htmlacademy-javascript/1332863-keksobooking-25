2048ab045a2e2d773a91a5721d832470c430c90d
Ciberian <isi04047891@gmail.com>
2022-04-06 16:49:22 +0500

    удаляет прежний код

--- js/map-filters.js
+++ js/map-filters.js
@@ -4,7 +4,6 @@ import { debounce } from './util.js';
 
 const SIMILAR_ADS_COUNT = 10;
 const DEBOUNCE_TIMEOUT = 500;
-const NON_CHECKBOX_FILTERS = 4;
 
 const mapForm = document.querySelector('.map__filters');
 
@@ -18,54 +17,43 @@ getData()
   .then(() => renderAds(getFirstTenAds()));
 
 const priceRange = {
+  any: {min: 0, max: Infinity},
   low: { min: 0, max: 10000 },
   middle: { min: 10001, max: 50000 },
   high: { min: 50001, max: Infinity },
 };
 
-const hasAllFeatures = (featuresArr, filterArr) => {
-  if (featuresArr) {
-    let match = 0;
-    for (let i = NON_CHECKBOX_FILTERS; i < filterArr.length; i++) {
-      if (featuresArr.includes(filterArr[i])) {
-        match++;
-      }
-    }
-
-    if (match === filterArr.length - NON_CHECKBOX_FILTERS) {
-      return true;
-    }
-
-    return false;
+const checkAdProperties = (filterName, filterValue, adObj) => {
+  if (filterName === 'housing-price') {
+    return adObj.offer.price >= priceRange[filterValue].min && adObj.offer.price <= priceRange[filterValue].max;
   }
 
-  return false;
+  if (filterName === 'features') {
+    return adObj.offer.features ? adObj.offer.features.includes(filterValue) : false;
+  }
+
+  return filterValue === String(adObj.offer[filterName.replace('housing-', '')]) || filterValue === 'any';
 };
 
 const renderFilteredAds = () => {
 
-  const activeFilters = [];
+  const activeFilters = {};
 
-  for (const formValue of new FormData(mapForm).entries()) {
-    activeFilters.push(formValue[1]);
-  }
+  const filteredAds = ads.filter((ad) => {
 
-  const filterAds = (ad) => (
-    (activeFilters[0] === 'any' || activeFilters[0] === ad.offer.type) &&
-    (activeFilters[1] === 'any' || (ad.offer.price >= priceRange[activeFilters[1]].min && ad.offer.price <= priceRange[activeFilters[1]].max)) &&
-    (activeFilters[2] === 'any' || Number(activeFilters[2]) === ad.offer.rooms) &&
-    (activeFilters[3] === 'any' || Number(activeFilters[3]) === ad.offer.guests) &&
-    (activeFilters.length > NON_CHECKBOX_FILTERS ? hasAllFeatures(ad.offer.features, activeFilters) : true)
-  );
+    for (const formValue of new FormData(mapForm).entries()) {
+      activeFilters[formValue[0]] = checkAdProperties(...formValue, ad);
+    }
 
-  const filteredAds = ads.filter((ad) => filterAds(ad));
+    if (!Object.values(activeFilters).includes(false))  {
+      return true;
+    }
 
+    return false;
+  });
 
-  if (activeFilters.every((filter) => filter === 'any')) {
-    renderAds(getFirstTenAds());
-  } else {
-    renderAds(filteredAds.slice(0, SIMILAR_ADS_COUNT));
-  }
+  renderAds(filteredAds.slice(0, SIMILAR_ADS_COUNT));
+
 };
 
 mapForm.addEventListener('change', debounce(renderFilteredAds, DEBOUNCE_TIMEOUT));
